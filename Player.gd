bfb5da467e1a2474429e8d1ffc2d0b4189fb1f59
Arthur Medeiros <rthurmedeiros@gmail.com>
2020-12-05 11:27:15 -0300

    Initial commit

---
+++ Player.gd
@@ -0,0 +1,29 @@
+extends KinematicBody2D
+
+const SPEED = 100
+
+func _process(delta):
+	if Input.is_action_just_released("quit"):
+		get_tree().quit()
+	if Input.is_action_pressed("reset"):
+		get_tree().reload_current_scene()
+
+func _physics_process(delta):
+	var velocity = Vector2()
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	elif Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	
+	move_and_collide(velocity.normalized() * SPEED * delta)
+	
+	if velocity == Vector2():
+		$AnimatedSprite.play("idle")
+	else:
+		$AnimatedSprite.play("down")
